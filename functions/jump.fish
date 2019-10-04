bceca6ca8af041b348e43e60227d2a7a8616ec7b
Kevin Lane <lane.kevin.a@gmail.com>
2019-10-03 21:09:01 -0700

    Return if folder found in root mark to avoid unnecessary variable

--- functions/jump.fish
+++ functions/jump.fish
@@ -5,17 +5,14 @@ function jump
     if test -d $MARKPATH/$argv[1] -a -L $MARKPATH/$argv[1]
       cd (readlink $MARKPATH/$argv[1])
     else
-      set -l exists false
       for root in (command ls $MARKPATH | string match '*.root')
         if test -d $MARKPATH/$root -a -L $MARKPATH/$root -a -d $MARKPATH/$root/$argv[1]
-          set exists true
           cd (readlink $MARKPATH/$root)/$argv[1]
+          return
         end
       end
 
-      if not $exists
-        echo "No such mark: $argv[1]"
-      end
+      echo "No such mark: $argv[1]"
     end
   end
 end
