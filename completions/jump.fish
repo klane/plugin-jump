7bb88ad1339e98b5c504ab67c8bb5cb1041f0ecf
Kevin Lane <lane.kevin.a@gmail.com>
2019-10-03 21:20:44 -0700

    Update completions with support for root marks

--- completions/jump.fish
+++ completions/jump.fish
@@ -1,5 +1,7 @@
 # SYNOPSIS
 #   Completions for jump package.
 
-complete -c jump -f -a '(command ls ~/.marks)'
-complete -c unmark -f -a '(command ls ~/.marks)'
+complete -c jump -f -a "(command ls ~/.marks | string match --invert '*.root'
+                        and find ~/.marks/*.root/ -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)"
+complete -c unmark -n '__fish_not_contain_opt -s r root' -f -a '(command ls ~/.marks)'
+complete -c unmark -n '__fish_contains_opt -s r root' -f -a "(command ls ~/.marks | string replace --filter '.root' '')"
